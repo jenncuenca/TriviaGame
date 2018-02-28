90ec5e75e8c48dfb80289ffb2fab6074dbb38ef8
Jennifer Cuenca <>
2018-02-28 12:52:13 -0500

    updated game psuedo-code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
 
+//game variables
 var questions= [];
 var answers= [];
 
 var rightAnswers= 0;
-var wrongAnsers= 0;
+var wrongAnswers= 0;
 var unanswered= 0;
 
-//game variables
 
-//game questions
 
-//player can only choose one answer at a time
+//first question is chosen
+    // question options presented to player
+
+    //player can only choose one answer at a time
+
+    //player's answer is compared to correct answer
+
+    //if true answer is counted as a rightAnswer
+    
+    //if false counted as wrongAnswer
+
+    //question timed 
+
 
 //win-lose conditions
 
 //game reset
 
-//timer
 
 }
 
