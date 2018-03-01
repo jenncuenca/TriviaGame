e9fbf084550ebc054dbe860193f1ef9e4bfdb3f0
Jennifer Cuenca <>
2018-03-01 16:49:05 -0500

    added questions to js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
 
 //game variables
-var questions= [];
-var answers= [];
+var questions= [{
+        question:"What spell is used to levitate objects?",
+        answerOptions: ["Alohomora","Wingardium Leviosa", "Levicorpus", "Protego"],
+        answer: 1
+    },{
+        question:"What spell would you use to summon an object to you?",
+        answerOptions: ["Alohomora","Levicorpus","Accio","Crucio"],
+        answer: 2
+    },{
+        question:"What is the counterspell for Lumos?",
+        answerOptions:["Noir","Delumos","Evanesco","Nox"],
+        answer: 3
+        },{
+        question:"What spell could be used to erase memories from an individual's mind?",
+        answerOptions: ["Obliviate", "Aguamenti", "Accio", "Levicorpus"],
+        answer: 0
+    },{
+        question:"What spell would you use to protect yourself against Dementors?",
+        answerOptions:["Incendio","Protego","Expecto Patronum","Evanesco"],
+        answer: 1
+    },{
+        question:"What spell can be used to disarm an attacker?",
+        answerOptions:["Protego","Expulso","Expelliarmus","Imperio"],
+        answer: 2
+        },{
+        question:"What spell is not one of the 3 Unforgivavle Curses?",
+        answerOptions: ["Imperio","Avada Kedavra", "Sectumsempra","Crucio"],
+        answer: 2
+        }
+
+}];
 
 var rightAnswers= 0;
 var wrongAnswers= 0;
